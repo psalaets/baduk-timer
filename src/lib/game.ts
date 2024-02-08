f79e040ac57458debfbd2af12715bf6146e39e11
Paul Salaets <psalaets@gmail.com>
2024-02-07 19:37:28 -0500

    Only make a new clock if settings have really changed

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -1,6 +1,6 @@
 import { derived, writable, get } from 'svelte/store';
 import type { ClockSettings } from './timing/clock-settings';
-import { create as createClock } from './timing/dual-clock';
+import { create as createClock, type GameClock } from './timing/dual-clock';
 import type { Color } from '$lib/color';
 
 const gameSettings = writable<ClockSettings | null>(null);
@@ -17,14 +17,38 @@ export const settings = {
   }
 };
 
-const gameClock = derived([gameSettings], ([settings]) => {
-  if (settings) {
-    const clock = createClock(settings);
-    return clock;
-  } else {
-    return null;
-  }
-});
+/**
+ * Need to cache previous value and only update if new value is not equal
+ * to previous value. The equals check should be a function.
+ */
+const settingsEqual = (current: ClockSettings, prev: ClockSettings | null) => {
+  if (prev == null) return false;
+
+  if (current.type !== prev.type) return false;
+  if (current.mainTimeSeconds !== prev.mainTimeSeconds) return false;
+  if (current.periods !== prev.periods) return false;
+  if (current.timePerPeriodSeconds !== prev.timePerPeriodSeconds) return false;
+
+  return true;
+};
+
+let previousSettings: ClockSettings | null = null;
+const gameClock = derived<typeof gameSettings, GameClock | null>(
+  gameSettings,
+  (settings, set) => {
+    if (settings) {
+      if (!settingsEqual(settings, previousSettings)) {
+        const clock = createClock(settings);
+        set(clock);
+      }
+    } else {
+      set(null);
+    }
+
+    previousSettings = settings;
+  },
+  null
+);
 
 const paused = writable(true);
 paused.subscribe((isPaused) => {
