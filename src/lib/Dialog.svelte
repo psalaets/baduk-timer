b45b832592e5b5ad9f233257cf07c7f5f4f1cfac
Paul Salaets <psalaets@gmail.com>
2024-02-09 11:02:22 -0500

    Resolve issue where a link in the dialog was causing a full page reload.
    The dialog was stopping event propagation which prevented kit from
    using pushstate to handle the link click -- the browser's default
    behavior was allowed to happen.
    
    The stop propagation was in place to prevent clicks within the dialog
    content from being interpretted as clicking outside the dialog.
    
    It has been tweaked so the only way to click the dialog is by clicking
    its ::backdrop, which really is clicking outside the dialog.

--- src/lib/Dialog.svelte
+++ src/lib/Dialog.svelte
@@ -1,22 +1,52 @@
 <!-- Adapted from https://svelte.dev/examples/modal -->
 
 <script lang="ts">
+  import type { MouseEventHandler } from 'svelte/elements';
+  import { onMount } from 'svelte';
+
   let dialog: HTMLDialogElement;
 
-  $: if (dialog) dialog.showModal();
-  $: close = dialog ? dialog.close : () => {};
+  onMount(() => {
+    dialog.showModal();
+  });
+
+  const close = () => dialog.close();
+
+  const onClick: MouseEventHandler<HTMLDialogElement> = (event) => {
+    // If click was on the dialog itself then it was a click on the ::backdrop (see 1 and 2).
+    // In other words, user clicked outside of dialog
+    if (event.target === dialog) {
+      close();
+    }
+  };
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
-<dialog bind:this={dialog} on:close on:click|self={() => dialog.close()}>
-  <!-- svelte-ignore a11y-no-static-element-interactions -->
-  <div on:click|stopPropagation>
+<dialog bind:this={dialog} on:close on:click={onClick}>
+  <div class="inner">
     <slot name="title" />
     <hr />
-    <slot name="content" />
+    <slot name="body" />
     <hr />
     <slot name="footer" {close} />
-    <!-- svelte-ignore a11y-autofocus -->
-    <button autofocus on:click={() => dialog.close()}>close modal</button>
   </div>
 </dialog>
+
+<style>
+  dialog {
+    /* (1) Makes child fill 100% of dialog height */
+    display: flex;
+    /* (2) The dialog has no padding and its child completely fills the content
+    box (see 1) so the only way to really click dialog is clicking its ::backdrop
+    and those clicks can be treated as user clicking outside of dialog.
+
+    dialog's padding is provided by its child (see 3)
+    */
+    padding: 0;
+  }
+
+  .inner {
+    /* (3) padding for all content within the dialog */
+    padding: 1rem;
+  }
+</style>
--- src/lib/pause/PauseDialog.svelte
+++ src/lib/pause/PauseDialog.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
   import Dialog from '$lib/Dialog.svelte';
 </script>
 
 <Dialog on:close>
   <span slot="title">Paused</span>
-  <div slot="content">
-    <ul>
-      <li>New game</li>
-    </ul>
+  <div slot="body">
+    <a href="/new">new game</a>
   </div>
   <div slot="footer" let:close>
-    <button on:click={() => close()}>Resume</button>
+    <button on:click={close}>Resume</button>
   </div>
 </Dialog>
