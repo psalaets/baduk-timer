681eb23c6ab1ab589cc9a61a7eb9a866d520a00b
Paul Salaets <psalaets@gmail.com>
2024-02-25 21:58:31 -0500

    Clean up

--- src/lib/in-game/InGame.svelte
+++ src/lib/in-game/InGame.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
   import type { Game } from '$lib/game';
   import GameClock from '$lib/game-clock/GameClock.svelte';
-  import type { Color } from '$lib/color';
   import PauseDialog from '$lib/pause/PauseDialog.svelte';
 
   export let game: Game;
 
   const { started, paused, clockState } = game;
-
-  function onStone(color: Color) {
-    game.stonePlayed(color);
-  }
-
-  function onPause() {
-    game.pause();
-  }
-
-  function onResume() {
-    game.resume();
-  }
 </script>
 
 <div>started: {$started}</div>
@@ -27,10 +14,10 @@
 <GameClock
   gameClock={$clockState}
   settings={game.settings}
-  on:stone={(event) => onStone(event.detail)}
+  on:stone={(event) => game.stonePlayed(event.detail)}
 >
-  <button on:click={() => onPause()}>Pause</button>
+  <button on:click={() => game.pause()}>Pause</button>
   {#if $started && $paused}
-    <PauseDialog on:close={() => onResume()} />
+    <PauseDialog on:close={() => game.resume()} />
   {/if}
 </GameClock>
