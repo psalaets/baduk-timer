f85fd262ae41af50f5bd02cf93311f721c1d73c5
Paul Salaets <psalaets@gmail.com>
2024-02-25 21:47:57 -0500

    No longer needed, the clockfaces are enough

--- src/lib/in-game/InGame.svelte
+++ src/lib/in-game/InGame.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import type { Game } from '$lib/game';
   import GameClock from '$lib/game-clock/GameClock.svelte';
-  import Preview from '$lib/preview/Preview.svelte';
   import type { Color } from '$lib/color';
   import PauseDialog from '$lib/pause/PauseDialog.svelte';
 
@@ -20,25 +19,18 @@
   function onResume() {
     game.resume();
   }
-
-  function onBegin() {
-    game.begin();
-  }
 </script>
 
 <div>started: {$started}</div>
 <div>paused: {$paused}</div>
-{#if $started}
-  <GameClock
-    gameClock={$clockState}
-    settings={game.settings}
-    on:stone={(event) => onStone(event.detail)}
-  >
-    <button on:click={() => onPause()}>Pause</button>
-    {#if $paused}
-      <PauseDialog on:close={() => onResume()} />
-    {/if}
-  </GameClock>
-{:else}
-  <Preview settings={game.settings} on:begin={() => onBegin()} />
-{/if}
+
+<GameClock
+  gameClock={$clockState}
+  settings={game.settings}
+  on:stone={(event) => onStone(event.detail)}
+>
+  <button on:click={() => onPause()}>Pause</button>
+  {#if $started && $paused}
+    <PauseDialog on:close={() => onResume()} />
+  {/if}
+</GameClock>
