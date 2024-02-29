fb47ddfb44ae1aed30260b6bd0ce472a24edd6d8
Paul Salaets <psalaets@gmail.com>
2024-02-29 09:04:27 -0500

    Clean up

--- src/lib/new-game/common-fields.ts
+++ src/lib/new-game/common-fields.ts
@@ -4,7 +4,6 @@ import {
   parseType,
   DEFAULT_TYPE
 } from '$lib/clock-settings/common-settings';
-import { BYOYOMI, CANADIAN, FISCHER, type ClockType } from '$lib/clock-settings/clock-type';
 import { firstFullyPopulated } from '$lib/util/first';
 import { getter } from './get-form-value';
 import type { RawValues } from '$lib/util/raw-values';
@@ -20,8 +19,6 @@ export function parse(formData: FormData): CommonSettings {
     type: parseType(get('timeSystem'))
   };
 }
-
-// TODO should initial values be a merge pipeline? or just use the first object that has all values?
 
 export function getInitialValues(): RawValues<CommonSettings> {
   const raw = firstFullyPopulated([
