db5ac7fadbf6f8d8382ae6a5189c7c3a82faed4e
Cameron Leslie <cameronleslie@gmail.com>
2024-06-05 14:41:10 -0700

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <svelte:head>
   <title>BadukTimer</title>
-  <meta name="descriptions" content="Game clock for Go / Baduk / Weiqi / Igo" />
+  <meta name="description" content="Game clock for Go / Baduk / Weiqi / Igo" />
 </svelte:head>
 
 <slot />
