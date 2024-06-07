eccbcf4fe01c3cb973267ee3eda7db8cc1f33010
Paul Salaets <psalaets@gmail.com>
2024-06-06 22:34:19 -0500

    Try to prevent extra beep when time is urgent and user just canceled out of new game form to return to paused game clocks.

--- src/lib/game-effects/urgent-time-sfx.ts
+++ src/lib/game-effects/urgent-time-sfx.ts
@@ -1,6 +1,7 @@
 import { derived } from 'svelte/store';
 import { type Game } from '$lib/game';
 import * as sfx from '$lib/game-effects/sfx';
+import type { GameClockState } from '$lib/timing/game-clock';
 
 export function urgentTimeSfx(game: Game) {
   // These ranges seem a bit weird but they're needed to get 5 boops of each urgency.
@@ -12,26 +13,47 @@ export function urgentTimeSfx(game: Game) {
     if (isUrgent(secondsLeft)) sfx.urgent();
   };
 
-  const blackUrgentSecondsLeft = derived<[typeof game.clockState, typeof game.paused], number>(
-    [game.clockState, game.paused],
-    ([clockState, paused], set) => {
-      if (!paused) {
-        set(Math.trunc(clockState.black.countdown));
-      }
-    }
-  );
+  type ClockStateStore = typeof game.clockState;
+  type PausedStore = typeof game.paused;
+  type CountdownSelector = (clockState: GameClockState) => number;
 
+  const urgentSecondsStore = (
+    clockStateStore: ClockStateStore,
+    pausedStore: PausedStore,
+    selector: CountdownSelector
+  ) => {
+    let previousSecondsLeft: number | null = null;
+
+    return derived<[ClockStateStore, PausedStore], number>(
+      [clockStateStore, pausedStore],
+      ([clockState, paused], set) => {
+        if (!paused) {
+          const currentSecondsLeft = Math.trunc(selector(clockState));
+
+          // Update current seconds only if it's different than last time's seconds
+          if (previousSecondsLeft !== null && currentSecondsLeft !== previousSecondsLeft) {
+            set(currentSecondsLeft);
+          }
+          previousSecondsLeft = currentSecondsLeft;
+        }
+      }
+    );
+  };
+
+  // watch black urgency
+  const blackUrgentSecondsLeft = urgentSecondsStore(
+    game.clockState,
+    game.paused,
+    (clockState) => clockState.black.countdown
+  );
   const unsubBlackUrgency = blackUrgentSecondsLeft.subscribe(playUrgencySound);
 
-  const whiteUrgentSecondsLeft = derived<[typeof game.clockState, typeof game.paused], number>(
-    [game.clockState, game.paused],
-    ([clockState, paused], set) => {
-      if (!paused) {
-        set(Math.trunc(clockState.white.countdown));
-      }
-    }
+  // watch white urgency
+  const whiteUrgentSecondsLeft = urgentSecondsStore(
+    game.clockState,
+    game.paused,
+    (clockState) => clockState.white.countdown
   );
-
   const unsubWhiteUrgency = whiteUrgentSecondsLeft.subscribe(playUrgencySound);
 
   return () => {
